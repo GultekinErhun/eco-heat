5694c1eed5fafdd50a68ff8c3f661157343985ac
ErhunGultekin <erhun.gultekin@datalobster.io>
2025-05-21 19:17:19 +0300

    urls updated

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sensors.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include('schedules.urls')),  # Schedules uygulamasının URL'lerini ekleyin
+
 ]
+
